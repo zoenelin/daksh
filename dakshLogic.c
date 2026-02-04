51e678b8f3959e449eb9f39d864e2f5347de0a2f
Zoe Nelin <zoe.nelin@me.com>
2026-02-04 17:58:59 -0600

    v2

--- dakshLogic.c
+++ dakshLogic.c
@@ -11,17 +11,40 @@
 */
 
 void interactive(void){
-    char *input = NULL; // allocates a block of memory for buffer
-    size_t input_buf_size = 0; // holds the size of the buffer
+    char *input = NULL; // allocates a block of memory for buffer // malloc(sizeof(char) * input_buf_size);
+    size_t input_buf_size = 0; // holds the size of the buffer and initialized to 0
 
   while(1) {  
     printf("daksh> ");
+    // read input 
     getline(&input, &input_buf_size, stdin); // (address of buffer, address of size of buffer, standard input stream --> keyboard)
-    printf("\n");
-    printf("Command typed: %sWith buffer size of %ld\n", input, input_buf_size);
-  
+    
+    // exit while loop condition
     if (strcmp(input, "exit\n") == 0){
       break;
+    }
+
+    printf("Command typed: %sWith buffer size of %ld\n", input, input_buf_size);
+
+    // char * strsep(char **stringp, const char *delim);
+    // parse input 
+    char *parse_input = input;
+    char *token;  // token is spaces (" ")
+    char *argv[25];
+    int argc = 0;
+
+    while ((token = strsep(&parse_input, " \t\n")) != NULL) {
+      if (*token ==  '\0') {
+        continue; // skip over empty tokens
+      }
+      argv[argc++] = token;
+    }
+
+    argv[argc] = NULL; // required for execvp later
+    
+    // debugging purposes
+    for (int i = 0; i < argc; i++) {
+      printf("Token[%d]: %s\n", i, argv[i]);
     }
   } 
   free(input);
